58f5e247d6bbd9ec0f090803826c592275729679
Brian-15 <bcirieco@gmail.com>
2021-09-24 16:24:07 -0700

    implemented starOutGrid.

--- star-out-grid.js
+++ star-out-grid.js
@@ -1,2 +1,22 @@
 function starOutGrid(grid) {
+
+    const result = grid.map(row => row.map(el => el));
+
+    for (let i in grid) {
+        
+        starIndex = grid[i].indexOf('*')
+        console.log(starIndex)
+
+        if (starIndex !== -1) {
+            for (let j in result) {
+                result[j][starIndex] = '*'
+            }
+
+            result[i].fill('*')
+        }
+    }
+
+
+    return result;
+
 }
