6cee787485cba42a1df6da1101821d859acc3d04
Brian-15 <bcirieco@gmail.com>
2021-09-24 15:20:25 -0700

    implemented snakeToCamel

--- snake-to-camel.js
+++ snake-to-camel.js
@@ -1,2 +1,23 @@
-function snakeToCamel() {}
+function snakeToCamel(str) {
+
+    const arr = [];
+    let isCapitalized = false;
+
+    for (let char of str) {
+        if (char === '_') {
+            isCapitalized = true;
+            continue;
+        }
+
+        if (isCapitalized) {
+            arr[arr.length] = char.toUpperCase();
+            isCapitalized = false;
+        }
+        else {
+            arr[arr.length] = char
+        }
+    }
+
+    return arr.join('')
+}
 
